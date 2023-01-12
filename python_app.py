cad67c688da81be08efa656b17e84487d8302889
KarLOL82 <klinfeldt@gmail.com>
2023-01-11 20:41:19 -0800

    file structure and basic function

---
+++ python_app.py
@@ -0,0 +1,15 @@
+def main():
+    word_dictionary = {
+        "hi": "A common greeting",
+        "eyes": "An organ for seeing",
+        "earth": "A planet in space"
+
+    }
+
+    word = input("Enter a word: ")
+    if word == "":
+        break
+    if word in word_dictionary:
+        print(word, ":", word_dictionary[word])
+
+main()
