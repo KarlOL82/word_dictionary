62d9db466aa0a3439bcd95988f3728abbf702485
KarLOL82 <klinfeldt@gmail.com>
2023-01-12 20:04:03 -0800

    working dictionary with local word dictionary

--- python_app.py
+++ python_app.py
@@ -7,8 +7,8 @@ def main():
     }
 
     word = input("Enter a word: ")
-    if word == "":
-        break
+    # if word == "":
+    #     break
     if word in word_dictionary:
         print(word, ":", word_dictionary[word])
 
